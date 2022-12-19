b3930847ab89ef45d5e08058f67f7cb37e9c3369
Pedro Freire <phfreire@id.uff.br>
2022-12-19 19:35:41 -0300

    feat: script to output portfolio history

--- scripts/queries.sql
+++ scripts/queries.sql
@@ -1,15 +1,20 @@
--- Visualize the portfolio
--- SELECT 
---     portfolios.name AS portfolio_name,
---     assets.name AS asset_name, 
---     stocks.type, 
---     stocks.ticker, 
---     stocks.sector, 
---     stocks.industry
--- FROM portfolio_transactions
--- INNER JOIN assets ON portfolio_transactions.asset_id = assets.id
--- INNER JOIN portfolios ON portfolio_transactions.portfolio_id = portfolios.id
--- INNER JOIN asset_types ON assets.type_id = asset_types.id
--- INNER JOIN stocks ON assets.id = stocks.asset_id
--- WHERE asset_types.name = 'stock';
--- It seems that this script is not working as intended
+-- Portfolios history
+SELECT
+    sh.date,
+    p.name AS portfolio_name, 
+    a.name AS asset_name, 
+    at.name AS type, 
+    sh.adjusted_close AS price, 
+    pa.asset_quantity AS quantity,
+    sh.adjusted_close * pa.asset_quantity AS value
+FROM portfolio_assets pa
+JOIN portfolios p ON pa.portfolio_id = p.id
+JOIN assets a ON pa.asset_id = a.id
+JOIN asset_types at ON a.type_id = at.id
+JOIN stock_history sh ON a.id = sh.stock_id
+JOIN (
+    SELECT portfolio_id, MAX(last_modified) as last_modified
+    FROM portfolio_assets
+    GROUP BY portfolio_id
+) t ON pa.portfolio_id = t.portfolio_id AND pa.last_modified = t.last_modified
+ORDER BY sh.date
