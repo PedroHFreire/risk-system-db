993050cb56e34a63095547018beb1cf606259b74
Pedro Freire <phfreire@id.uff.br>
2023-04-14 11:05:34 -0300

    Fix FileNotFoundError in first-stocks.py by providing correct CSV file paths

--- scripts/init/first-stocks.py
+++ scripts/init/first-stocks.py
@@ -1,13 +1,20 @@
 import csv
 import sqlite3
+import os
 
 # Connect to the database
 conn = sqlite3.connect('stable.db')
 cursor = conn.cursor()
 
+# Define the data folder path
+data_folder = os.path.join(os.path.dirname(__file__), 'data')
+
 # Iterate over the CSV files
 for filename in ['MSFT.csv', 'JNJ.csv', 'XOM.csv']:
-    with open(filename, 'r') as f:
+    # Construct the full path to the CSV file
+    file_path = os.path.join(data_folder, filename)
+    
+    with open(file_path, 'r') as f:
         # Use the csv module to read the file
         reader = csv.reader(f)
         # Skip the header row
@@ -15,7 +22,7 @@ for filename in ['MSFT.csv', 'JNJ.csv', 'XOM.csv']:
         # Iterate over the rows in the CSV file
         for row in reader:
             # Insert the row into the table
-            cursor.execute('INSERT INTO stock_history (stock_id, date, open, high, low, close, adjusted_close, volume) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', row)
+            cursor.execute('INSERT INTO stock_history (asset_id, date, open, high, low, close, adjusted_close, volume) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', row)
 
 # Commit the transaction
 conn.commit()
